61b4c2952619469db3ab63bb6036bf8543f0c066
Nathan Gaudouin <ngaudoui@c1r2p2.42lehavre.fr>
2025-03-19 15:55:09 +0100

    change using strlen from printf to strlen from libft

--- printf/ft_putnbr_base.c
+++ printf/ft_putnbr_base.c
@@ -6,21 +6,22 @@
 /*   By: ngaudoui <ngaudoui@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 00:45:36 by jmilesi           #+#    #+#             */
-/*   Updated: 2025/03/19 15:52:47 by ngaudoui         ###   ########.fr       */
+/*   Updated: 2025/03/19 15:54:34 by ngaudoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include "../libft.h"
 
-size_t	ft_stlen(const char *s)
-{
-	int	i;
+// size_t	ft_stlen(const char *s)
+// {
+// 	int	i;
 
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
+// 	i = 0;
+// 	while (s[i] != '\0')
+// 		i++;
+// 	return (i);
+// }
 
 int	ft_pn_b(long long n, char *base)
 {
@@ -29,7 +30,7 @@ int	ft_pn_b(long long n, char *base)
 	int			len;
 
 	len = 0;
-	base_length = ft_stlen(base);
+	base_length = ft_strlen(base);
 	if (n < 0)
 	{
 		len += write(1, "-", 1);
